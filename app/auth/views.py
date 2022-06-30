e90f445c5249d9cd8dccfddfffa0d8800aaf3db8
18294090 <18294090@qq.com>
2022-06-30 20:44:50 +0800

    6

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from flask_login import login_user,logout_user,login_required
@@ -77,54 +76,3 @@ def reg():
 def personal_details():
     return(render_template('auth/personal_details.html'))
 
-=======
-from flask import render_template, redirect, request, url_for, flash
-from . import auth
-from flask_login import login_user,logout_user,login_required
-from ..models import user
-from .forms import userlogin
-from .. import db
-import time
-
-
-@auth.route("/", methods=["POST", "GET"])
-def login():
-    form1 = userlogin()
-    if form1.validate_on_submit():
-        u = user.query.filter_by(username=form1.username.data).first()
-        id_number =  user.query.filter_by(id_number=form1.username.data).first()
-        if (u and u.verify_password(form1.password.data))  :
-            login_user(u, form1.remember_me.data)
-            next = request.args.get('next')
-            if not next or not next.startswith("/"):
-                next = url_for('main.index')
-                u.login_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-                db.session.commit()
-            return(redirect(next))
-        if id_number and id_number.verify_password(form1.password.data):
-            login_user(id_number, form1.remember_me.data)
-            next = request.args.get('next')
-            if not next or not next.startswith("/"):
-                next = url_for('main.index')
-                id_number.login_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
-                db.session.commit()
-            return(redirect(next))
-        flash("错误的账号或密码")
-    return(render_template("auth/login.html", form1=form1))
-
-
-@auth.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    flash("你已成功退出系统")
-    return(redirect(url_for('auth.login')))
-
-@auth.route("/reset/<id>")
-def reset(id):
-    u=user.query.filter(user.id==id).first()
-    u.password="123456"
-    db.session.flush()
-    db.session.commit()
-    return("重置成功")
->>>>>>> d989a01c055dd7066c1fb6cabda1c43d81584f09
--- app/job/forms.py
+++ app/job/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """网页视图上窗体的定义"""
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import PasswordField, SubmitField, SelectField, StringField, IntegerField, DateField
@@ -13,18 +12,3 @@ class publish(FlaskForm):
     subject = SelectField(label="学科", choices=subjects, validators=[DataRequired('确定学科')])
     job_name = StringField(label="作业名称", validators=[DataRequired('作业名称')])
     context = CKEditorField(label="备注")
-=======
-"""网页视图上窗体的定义"""
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import PasswordField, SubmitField, SelectField, StringField, IntegerField, DateField
-from flask_wtf import FlaskForm
-from flask_wtf.file import DataRequired
-from flask_ckeditor import CKEditorField
-from flask_codemirror.fields import CodeMirrorField
-from wtforms.validators import InputRequired, EqualTo
-subjects =[(1, '语文'), (2, '数学'), (3, '外语'), (4, '政治'), (5, '历史'),(6, '地理'), (7, '物理'), (8, '化学'), (9, '生物'), (9, '信息技术'), (10, '通用技术'), (11, '音乐'), (12, '体育'), (13, '美术'), (14, '科学')]
-
-class publish(FlaskForm):
-    subject = SelectField(label="学科", choices=subjects, validators=[DataRequired('确定学科')])
-    job_name = StringField(label="作业名称", validators=[DataRequired('作业名称')])
->>>>>>> d989a01c055dd7066c1fb6cabda1c43d81584f09
